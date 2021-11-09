b0579d2738ef90a79f82c7a5bc5a4138ffa4aedd
Matt Roden <mattroden92@gmail.com>
2021-11-08 20:41:34 -0700

    Finish writing and testing necessary api calls - no error handling yet

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,9 +5,15 @@ import { getHomeArticles, getArticlesByTopic } from '../../utils'
 function App() {
   const [articles, setArticles] = useState([])
 
+  const onTopicSearch = (topic) => {
+    getArticlesByTopic(topic).then(data => setArticles(data.results))
+    // This will need to get passed down to the topic selector component
+    // the value of the selected topic will be passed back up and strung through as arg
+  }
+
   useEffect(() => {
     getHomeArticles().then(data => setArticles(data.results))
-    getArticlesByTopic('books').then(data => console.log("Book Articles: ", data.results))
+    // getArticlesByTopic('books').then(data => console.log("Book Articles: ", data.results))
   }, [])
 
   return (
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,4 @@
 const API_KEY = "GsNXNQlz60M6Dc1Pum7oDqRHwAexkGq2"
-const base_url = "https://api.nytimes.com/svc/topstories/v2/TOPIC.json?api-key=yourkey"
 const homePageArticlesURL = "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=GsNXNQlz60M6Dc1Pum7oDqRHwAexkGq2"
 
 export const getHomeArticles = () => {

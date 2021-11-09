035a8f77413a8136d36655aff5ac876221c1eafb
Matt Roden <mattroden92@gmail.com>
2021-11-09 11:01:15 -0700

    Render article card data from container parent

--- src/components/CardsContainer/CardsContainer.js
+++ src/components/CardsContainer/CardsContainer.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import ArticleCard from '../ArticleCard/ArticleCard'
 
 const CardsContainer = () => {
+  //map out 
+
+
   return (
     <div className="articleCards--container">
       "Card Container Here"
+      <ArticleCard />
     </div>
   )
 }

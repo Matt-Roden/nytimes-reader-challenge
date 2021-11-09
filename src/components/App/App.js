d039735958ab5fb156eb1037304c7c342381f50b
Matt Roden <mattroden92@gmail.com>
2021-11-08 20:26:22 -0700

    Make successful api call for topic home

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,14 @@
+import React, { useEffect, useState} from 'react'
+
 import '../../styles/App.css';
+import { getHomeArticles } from '../../utils'
 
 function App() {
+
+  useEffect(() => {
+    getHomeArticles().then(data => console.log("Data: ", data.results))
+  }, [])
+
   return (
     <div className="App">
       NYTimes Reader
---
+++ src/utils.js
@@ -0,0 +1,9 @@
+const API_KEY = "GsNXNQlz60M6Dc1Pum7oDqRHwAexkGq2"
+const base_url = "https://api.nytimes.com/svc/topstories/v2/TOPIC.json?api-key=yourkey"
+const homePageArticlesURL = "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=GsNXNQlz60M6Dc1Pum7oDqRHwAexkGq2"
+
+export const getHomeArticles = () => {
+  return fetch(homePageArticlesURL)
+    .then(response => response.json())
+    .catch(err => console.log('Error: ', err))
+}

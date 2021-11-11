4ddfdb32c28abc26a81fb6e11ac095de9c4e0b9a
Matt Roden <mattroden92@gmail.com>
2021-11-11 10:29:30 -0700

    Add basic styling and responsiveness to detailed view

--- src/components/ArticleDetails/ArticleDetails.js
+++ src/components/ArticleDetails/ArticleDetails.js
@@ -1,5 +1,6 @@
 import { Link, Redirect } from 'react-router-dom'
 import React, { useState, useEffect } from 'react'
+import '../../styles/ArticleDetails.css'
 
 const ArticleDetails = ({ details, articles }) => {
   const [articleDetails, setArticleDetails] = useState(details)
@@ -25,9 +26,12 @@ const ArticleDetails = ({ details, articles }) => {
           <div className='titleBox--container'>
             <h1 className='detailsTitle--text'>{articleDetails.title}</h1>
             <p className='subHeader--text'>{articleDetails.abstract}</p>
+            <a href={`${articleDetails.url}`} target="_black">
+              <p>Link to Full Article</p>
+            </a>
           </div>
           
-          <img src={articleDetails.multimedia[0].url}></img>
+          <img className='detailsImage--img' src={articleDetails.multimedia[0].url} alt={articleDetails.multimedia[0].caption}></img>
 
         </div>
       )}
